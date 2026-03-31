a2229b1721b94dee7e3fff3b02c9f2a19c7bc4f2
gao <gaocampo2000@gmail.com>
2026-03-31 20:06:29 -0300

    Configuración para Despliegue II

--- veterinariaweb/settings.py
+++ veterinariaweb/settings.py
@@ -34,10 +34,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
